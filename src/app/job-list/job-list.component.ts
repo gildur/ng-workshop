e6ecea2c4391c8e89e61ddbc1d0aedc27670d242
Piotr Wolny <gildur@gmail.com>
2018-12-04 10:37:09 +0100

    Exercise 3. Displaying data.

--- src/app/job-list/job-list.component.ts
+++ src/app/job-list/job-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Job } from './job';
 
 @Component({
   selector: 'app-job-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobListComponent implements OnInit {
 
+  jobs: Job[];
+
   constructor() { }
 
   ngOnInit() {
+    this.jobs = [
+      { code: 'test1', name: 'Test 1', description: 'Lorem ipsum', validFrom: '2018-11-30' },
+      { code: 'test2', name: 'Test 2', description: 'Lorem ipsum', validFrom: '2018-12-02' },
+      { code: 'test3', name: 'Test 3', description: 'Lorem ipsum', validFrom: '2018-12-06' },
+    ];
   }
-
 }
---
+++ src/app/job-list/job.ts
@@ -0,0 +1,6 @@
+export class Job {
+  code: string;
+  name: string;
+  description: string;
+  validFrom: string;
+}
